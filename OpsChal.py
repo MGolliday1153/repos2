bbbeb5218ce64ad167fb93eb58d5366fede6306a
Matthew Golliday <matthewgolliday4@gmail.com>
2023-02-08 18:07:05 -0600

    301 Ops Challenge 6

---
+++ OpsChal.py
@@ -0,0 +1,23 @@
+#!bin/bash
+
+#Ops Challenge 6
+# Author: Matthew Golliday
+# Date: 2/7/2023
+
+import os
+
+#Declare variables
+define_user= os.popen("whoami")
+define_ip= os.popen("ip' 'A")
+define_hardware= os.popen("lshw' '-short")
+# Main printing
+print ("Result of whoami")
+print (define_user.read())
+
+print ("Result of ip a")
+print (define_ip.read())
+
+print ("Result of hardware")
+print (define_hardware.read())
+
+#End
